eb26c7ec7ab8c4d20034c8f5b6fdbdade4f062a8
santiagogui1909 <santiagogui1909@gmail.com>
2021-01-08 18:49:38 -0500

    subo parte de el proyecto seccion hombre

---
+++ src/js/Hombre.js
@@ -0,0 +1,325 @@
+/** base de datos */
+
+const relojes = [{
+        nombre: 'B-S-Negro',
+        imagen: 'https://i.ibb.co/tHvWfD9/B-S-Negro.jpg'
+    },
+    {
+        nombre: 'B-S-Quartz-Plateado',
+        imagen: 'https://i.ibb.co/Kxb407N/B-S-Quartz-Plateado.jpg'
+    },
+    {
+        nombre: 'Casio-Gris',
+        imagen: 'https://i.ibb.co/y0KnVHh/Casio-Gris.jpg'
+    },
+    {
+        nombre: 'Casio-Quartz-Plateado',
+        imagen: 'https://i.ibb.co/Pc2GYXX/Casio-Quartz-Plateado.jpg'
+    },
+    {
+        nombre: 'Casio-Quartz',
+        imagen: 'https://i.ibb.co/48z2FFm/Casio-Quartz.jpg'
+    },
+    {
+        nombre: 'COOL-Time-Verde',
+        imagen: 'https://i.ibb.co/nP6xc78/COOL-Time-Verde.jpg'
+    },
+    {
+        nombre: 'Diesel-5-Bar',
+        imagen: 'https://i.ibb.co/GnTHkjq/Diesel-5-Bar.jpg'
+    },
+    {
+        nombre: 'Fossil-Azul',
+        imagen: 'https://i.ibb.co/q7ycsd4/Fossil-Azul-2.jpg'
+    },
+    {
+        nombre: 'Fossil-Azul',
+        imagen: 'https://i.ibb.co/L9QcLzH/Fossil-Azul.jpg'
+    },
+    {
+        nombre: 'Fossil-Militar',
+        imagen: 'https://i.ibb.co/vzQmFfn/Fossil-Militar.jpg'
+    },
+    {
+        nombre: 'Fossil-Plateado',
+        imagen: 'https://i.ibb.co/RgG0j0r/Fossil-Plateado-2.jpg'
+    },
+    {
+        nombre: 'Fossil-Plateado',
+        imagen: 'https://i.ibb.co/sVRf6g3/Fossil-Plateado.jpg'
+    },
+    {
+        nombre: 'G-FRC Gold WR 50',
+        imagen: 'https://i.ibb.co/pX95ShL/G-FRC.jpg'
+    },
+    {
+        nombre: 'G-Shock-Casio-Plateado',
+        imagen: 'https://i.ibb.co/qYkwq3p/G-Shock-Casio-Plateado.jpg'
+    },
+    {
+        nombre: 'G-Shock-Casio-Protection',
+        imagen: 'https://i.ibb.co/Lpgfj3W/G-Shock-Casio-Protection.jpg'
+    },
+    {
+        nombre: 'G-Shock-Casio-White-Black',
+        imagen: 'https://i.ibb.co/qWQnvJp/G-Shock-Casio-White-Black.jpg'
+    },
+    {
+        nombre: 'G-Shock-Casio',
+        imagen: 'https://i.ibb.co/s5RKpfy/G-Shock-Casio.jpg'
+    },
+    {
+        nombre: 'Q-Q-Quartz-Sport',
+        imagen: 'https://i.ibb.co/QXS04YV/Q-Q-Quartz-Sport.jpg'
+    },
+    {
+        nombre: 'Saint-G-Sports',
+        imagen: 'https://i.ibb.co/3BZQXQg/Saint-G-Sports.jpg'
+    },
+    {
+        nombre: 'Sanse-X-Sport',
+        imagen: 'https://i.ibb.co/JHW8J15/Sanse-X-Sport.jpg'
+    },
+    {
+        nombre: 'Sanse-X-Sports',
+        imagen: 'https://i.ibb.co/z6JkPSt/Sanse-X-Sports.jpg'
+    },
+    {
+        nombre: 'Sport-Watch-Xinjia',
+        imagen: 'https://i.ibb.co/PttsPx4/Sport-Watch-Xinjia.jpg'
+    },
+    {
+        nombre: 'Vicolle-Wr',
+        imagen: 'https://i.ibb.co/pzvZZ9C/Vicolle-Wr.jpg'
+    },
+    {
+        nombre: 'Vicollo',
+        imagen: 'https://i.ibb.co/rHdxpg4/Vicollo-2.jpg'
+    },
+    {
+        nombre: 'Vicollo',
+        imagen: 'https://i.ibb.co/GkBp12n/Vicollo.jpg'
+    },
+    {
+        nombre: 'X-Sports',
+        imagen: 'https://i.ibb.co/KGB5pz7/X-Sports.jpg'
+    }
+];
+
+const billeteras = [{
+        imagen: 'https://i.ibb.co/BswBzGt/Billetera-Adidas.jpg'
+    },
+    {
+        imagen: 'https://i.ibb.co/v1R2jF3/Billetera-Puma.jpg'
+    },
+    {
+        imagen: 'https://i.ibb.co/v1R2jF3/Billetera-Puma.jpg"'
+    },
+    {
+        imagen: 'https://i.ibb.co/cK9wHMk/IMG-20210104-172306.jpg'
+    },
+    {
+        imagen: 'https://i.ibb.co/cK9wHMk/IMG-20210104-172306.jpg'
+    },
+    {
+        imagen: 'https://i.ibb.co/qxkZqbg/IMG-20210104-172324.jpg'
+    },
+    {
+        imagen: 'https://i.ibb.co/xKvWtwd/IMG-20210104-172335.jpg'
+    },
+    {
+        imagen: 'https://i.ibb.co/k5P7Kgn/IMG-20210104-172408.jpg'
+    },
+    {
+        imagen: 'https://i.ibb.co/0j4BmS7/IMG-20210104-172440.jpg'
+    }
+];
+
+const perfumes = [{
+        nombre: 'perfumes:',
+        imagen: 'https://picsum.photos/id/30/600'
+    },
+    {
+        nombre: 'perfumes:',
+        imagen: 'https://picsum.photos/id/30/600'
+    },
+    {
+        nombre: 'perfumes:',
+        imagen: 'https://picsum.photos/id/30/600'
+    },
+    {
+        nombre: 'perfumes:',
+        imagen: 'https://picsum.photos/id/30/600'
+    },
+    {
+        nombre: 'perfumes:',
+        imagen: 'https://picsum.photos/id/30/600'
+    },
+    {
+        nombre: 'perfumes:',
+        imagen: 'https://picsum.photos/id/30/600'
+    },
+    {
+        nombre: 'perfumes:',
+        imagen: 'https://picsum.photos/id/30/600'
+    },
+    {
+        nombre: 'perfumes:',
+        imagen: 'https://picsum.photos/id/30/600'
+    },
+    {
+        nombre: 'perfumes:',
+        imagen: 'https://picsum.photos/id/30/600'
+    },
+    {
+        nombre: 'perfumes:',
+        imagen: 'https://picsum.photos/id/30/600'
+    },
+    {
+        nombre: 'perfumes:',
+        imagen: 'https://picsum.photos/id/30/600'
+    },
+    {
+        nombre: 'perfumes:',
+        imagen: 'https://picsum.photos/id/30/600'
+    },
+    {
+        nombre: 'perfumes:',
+        imagen: 'https://picsum.photos/id/30/600'
+    },
+    {
+        nombre: 'perfumes:',
+        imagen: 'https://picsum.photos/id/30/600'
+    },
+    {
+        nombre: 'perfumes:',
+        imagen: 'https://picsum.photos/id/30/600'
+    }
+];
+
+
+
+/**variables */
+
+const reloj = document.querySelector("#productos-Relojes");
+const billetera = document.querySelector("#productos-Billeteras");
+const perfume = document.querySelector("#productos-Perfumes");
+
+
+function renderRelojes() {
+    for (let info of relojes) {
+
+        // Estructura
+        let miNodo = document.createElement('div');
+        miNodo.classList.add('card-reloj');
+        // Body
+        let miNodoCardBody = document.createElement('div');
+        miNodoCardBody.classList.add('card-body');
+        // Titulo
+        let miNodoTitle = document.createElement('h5');
+        miNodoTitle.classList.add('card-title');
+        miNodoTitle.textContent = info['nombre'];
+        // Imagen
+        let miNodoImagen = document.createElement('img');
+        miNodoImagen.classList.add('img-fluid');
+        miNodoImagen.setAttribute('src', info['imagen']);
+
+        miNodoCardBody.appendChild(miNodoImagen);
+        miNodoCardBody.appendChild(miNodoTitle);
+        miNodo.appendChild(miNodoCardBody);
+        reloj.appendChild(miNodo);
+
+    }
+}
+
+function renderPerfumes() {
+
+    for (let info of perfumes) {
+
+        let miNodo = document.createElement('div');
+        miNodo.classList.add('card-perfume');
+
+        let miNodoCardBody = document.createElement('div');
+        miNodoCardBody.classList.add('card-body');
+
+        let miNodoTitle = document.createElement('h5');
+        miNodoTitle.classList.add('card-title');
+        miNodoTitle.textContent = info['nombre'];
+
+        let miNodoImagen = document.createElement('img');
+        miNodoImagen.classList.add('img-fluid');
+        miNodoImagen.setAttribute('src',info['imagen']);
+
+        miNodoCardBody.appendChild(miNodoImagen);
+        miNodoCardBody.appendChild(miNodoTitle);
+        miNodo.appendChild(miNodoCardBody);
+        perfume.appendChild(miNodo);
+
+    }
+}
+
+function renderBilleteras() {
+    for (let info of billeteras) {
+        let miNodo = document.createElement('div');
+        miNodo.classList.add('card-billetera');
+
+        let miNodoCardBody = document.createElement('div');
+        miNodoCardBody.classList.add('card-body');
+
+        let miNodoImagen = document.createElement('img');
+        miNodoImagen.classList.add('img-fluid');
+        miNodoImagen.setAttribute('src', info['imagen']);
+
+        miNodoCardBody.appendChild(miNodoImagen);
+        miNodo.appendChild(miNodoCardBody);
+        billetera.appendChild(miNodo);
+    }
+}
+
+// $(document).ready(function () {        pedir ayuda con esto <<<<-------------------------------------------------------
+
+
+//     var btnReloj = $("#reloj");
+
+//     // var btnPerfume = $("#perfumme");
+
+//     var btnBilletera = $("#billetera");
+
+//     var reloj = $("#relojes");
+
+//     // var perfume = $("#perfumes");
+
+//     var billetera = $("#billeterasHombre");
+
+
+//     reloj.hide();
+//     // perfume.hide();
+//     billetera.hide();
+
+
+//     btnReloj.click(function () {
+//         reloj.show();
+//         perfume.hide();
+//         billetera.show();
+//     });
+
+//     // btnPerfume.click(function () {
+//     //     reloj.hide();
+//     //     perfume.show();
+//     //     billetera.hide();
+//     // });
+
+//     btnBilletera.click(function () {
+//         reloj.hide();
+//         perfume.hide();
+//         billetera.show();
+//     });
+
+// });
+
+
+renderRelojes();
+
+renderPerfumes();
+
+renderBilleteras();
