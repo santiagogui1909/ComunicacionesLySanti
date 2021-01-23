d70336e8a3343082ea87e0248d75731b0e305d44
CA2 <cristian2puntosv@gmail.com>
2021-01-22 19:14:40 -0500

    Añadi un indicador de carga, y una corta animación al presionar un link de la navegación interna de la página

--- src/js/joyeria.js
+++ src/js/joyeria.js
@@ -21,3 +21,11 @@ function uncheck() {
         }
     }
 }
+
+
+window.onload = function(){
+    var contenedor = document.getElementById('contenedor_carga');
+
+    contenedor.style.visibility = 'hidden';
+    contenedor.style.opacity = '0';
+}
