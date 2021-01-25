9ce3d28f294580bd7dcb8767b2a1d5807c06f94d
CA2 <cristian2puntosv@gmail.com>
2021-01-24 23:55:03 -0500

    Corregí algunos errores, y comencé a cambiar un poco el diseño

--- src/js/joyeria.js
+++ src/js/joyeria.js
@@ -1,31 +1,8 @@
-function uncheck() {
-    var checkbox1 = document.getElementById("btn-menu");
-    var checkbox2 = document.getElementById("btn-menu2");
-    var checkbox3 = document.getElementById("btn-menu3");
-    checkbox1.onclick = function () {
-        if (checkbox1.checked != false) {
-            checkbox2.checked = null;
-            checkbox3.checked = null;
-        }
-    }
-    checkbox2.onclick = function () {
-        if (checkbox2.checked != false) {
-            checkbox1.checked = null;
-            checkbox3.checked = null;
-        }
-    }
-    checkbox3.onclick = function(){
-        if (checkbox3.checked != false) {  
-            checkbox1.checked = null;
-            checkbox2.checked = null;
-        }
-    }
-}
-
-
 window.onload = function(){
     var contenedor = document.getElementById('contenedor_carga');
 
     contenedor.style.visibility = 'hidden';
     contenedor.style.opacity = '0';
 }
+
+
