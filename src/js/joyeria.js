29ae97826251a9c8ddef4d736330e4e47e1204ae
CA2 <cristian2puntosv@gmail.com>
2021-02-02 15:43:07 -0500

    Cambié el slider por uno manual-automatico

--- src/js/joyeria.js
+++ src/js/joyeria.js
@@ -1,6 +1,52 @@
+//Carga de la pagina 
 window.onload = function(){
     var contenedor = document.getElementById('contenedor_carga');
 
     contenedor.style.visibility = 'hidden';
     contenedor.style.opacity = '0';
 }
+
+// Slider
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function Next(){
+    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+function Back(){
+    let sliderSection = document.querySelectorAll(".slider__section");
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.marginLeft = "0";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+btnRight.addEventListener('click', function(){
+    Next();
+});
+
+btnLeft.addEventListener('click', function(){
+    Back();
+});
+
+setInterval( function(){
+    Next();
+}, 6000);
