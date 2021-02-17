32fcdc01036c24a16aa52cfd86fe9cca4f69e545
santiagogui1909 <santiagogui1909@gmail.com>
2021-02-17 16:04:47 -0500

    subo pagina contacto y modificaciones

--- src/js/MenuFooter.js
+++ src/js/MenuFooter.js
@@ -53,6 +53,24 @@ $("ul").click(function (valor){
 
 });
 
+
+(function($) {
+	"use strict"
+
+	// Mobile Nav toggle
+	$('.menu-toggle > a').on('click', function (e) {
+		e.preventDefault();
+		$('#responsive-nav').toggleClass('active');
+	})
+
+	// Fix cart dropdown from closing
+	$('.cart-dropdown').on('click', function (e) {
+		e.stopPropagation();
+	});
+
+
+})(jQuery);
+
 /**slider function */
 
 $(document).ready(function () {
